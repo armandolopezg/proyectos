ba34f97264f992ccd25cd5114e89db35041a6813
armandolopezg <coredrkv2@gmail.com>
2021-09-24 10:11:19 -0500

    actualizacion

---
+++ calculadora/hola.js
@@ -0,0 +1,10 @@
+function calcular(){
+    let num1= parseInt(document.getElementById("num-1").value;
+    let num2= parseInt(document.getElementById("num-2").value;
+    var opera= document.getElementById("Operacion").value;
+    if(opera==1){
+        document.getElementById("resultado").value= num1+num2;
+    }else{
+        document.getElementById("resultado").value=num1-num2;
+    }
+}
